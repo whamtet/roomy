f26e85ab2e61c46feb1cf06baa6c268f42b97f91
Matthew Molloy <whamtet@gmail.com>
2025-06-13 22:43:28 +0900

    change port

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -Dclojure.server.repl="{:port 5550 :accept clojure.core.server/repl}" -jar target/roomy-standalone.jar
+java -Dclojure.server.repl="{:port 5555 :accept clojure.core.server/repl}" -jar target/roomy-standalone.jar
